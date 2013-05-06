073d109184d3ca1e9dc49e1257e065436beb86af
Frank Lam <frank@almostflan.com>
2013-05-05 23:24:42 -0700

    Added lessons, cheat sheet, other slightly fancy things

--- lessons.js
+++ lessons.js
@@ -1,5 +1,9 @@
 var lessons = [
-    {label: "something", content: "blah blah blah"},
-    {label: "another something", content: "blah blah blah!"},
-    {label: "another something", content: "something that might be super long and take forever"}
+    {label: "1H #1: DTHE", content: "dddd tttt hhhh eeee the hed det ded thed thod eth ted ed he the hed deht theded the hed det ded thed thod eth ted ed he the hed deht theded the hed det ded thed thod eth ted ed he the hed deht theded the hed det ded thed thod eth ted ed he the hed deht theded"},
+    {label: "1H #2: CAUR", content: "cccc aaaa uuuu rrrr car that rated rut rat heart cart hurt rudder heat carted death udder car that rated rut rat heart cart hurt rudder heat carted death udder car that rated rut rat heart cart hurt rudder heat carted death udder car that rated rut rat heart cart hurt rudder heat carted death udder"},
+    {label: "1H #3: BYSO", content: "bbbb yyyy ssss oooo soy bay odd ore sore roar shudder shut rays boys bob robbed soy bay odd ore sore roar shudder shut rays boys bob robbed soy bay odd ore sore roar shudder shut rays boys bob robbed soy bay odd ore sore roar shudder shut rays boys bob robbed"},
+    {label: "1H #4: VWNI", content: "vvvv wwww nnnn iiii burns soybean very win nib wary weary inner winner ribbed vet burns soybean very win nib wary weary inner winner ribbed vet burns soybean very win nib wary weary inner winner ribbed vet burns soybean very win nib wary weary inner winner ribbed vet"},
+    {label: "1H #5: GKXZ", content: "gggg kkkk xxxx zzzz great crazy kick six stix gore gear kraut zany nix vixen zeta great crazy kick six stix gore gear kraut zany nix vixen zeta great crazy kick six stix gore gear kraut zany nix vixen zeta great crazy kick six stix gore gear kraut zany nix vixen zeta"},
+    {label: "1H #6: PFML", content: "pppp ffff mmmm llll pun flake milk mob film lips filter lemmings plane plums explore pun flake milk mob film lips filter lemmings plane plums explore pun flake milk mob film lips filter lemmings plane plums explore pun flake milk mob film lips filter lemmings plane plums explore"},
+    {label: "1H #7: JQ,.", content: "jjjj qqqq ,,,, .... jugs quack quote jail jobs. entertain quality, question things. more quotes can jiggle jams. jugs quack quote jail jobs. entertain quality, question things. more quotes can jiggle jams. jugs quack quote jail jobs. entertain quality, question things. more quotes can jiggle jams."}
 ];
